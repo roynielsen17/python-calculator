09bc999c007f55efdf6a4b2056d4dea5904bb4b0
victor <victor@victors-MacBook-Pro.local>
2026-09-24 09:09:13 -0600

    adding documentation

--- src/python-calculator/sexagesimal.py
+++ src/python-calculator/sexagesimal.py
@@ -2,6 +2,15 @@
 """
 Decimal ↔ pure alphanumeric sexagesimal converter
 Digit set: 0-9a-zA-X  (exactly 60 symbols)
+
+Sexigesimal is the base60 number system the Sumarians and Babalonians
+used.  
+
+https://en.wikipedia.org/wiki/Sexagesimal
+https://knowledgebasedsociety.com/sumerian-sexagesimal-system-and-time-invention/
+
+
+
 """
 
 import argparse
